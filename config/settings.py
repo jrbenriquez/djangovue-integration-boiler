aec39722989c26487f5c115c79d8eed00e8a2fcd
John Rei Enriquez <johnrei.enriquez@gmail.com>
2018-05-19 06:45:09 +0000

    Generic Project name

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'jrbenriquez.urls'
+ROOT_URLCONF = 'djangovue.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'jrbenriquez.wsgi.application'
+WSGI_APPLICATION = 'djangovue.wsgi.application'
 
 
 # Database
